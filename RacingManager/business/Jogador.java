910acc467448e6226200553fc6966fa6adcdf01f
SD <100542887+JBB-DO-MAL-V2@users.noreply.github.com>
2022-12-20 17:50:58 +0000

    fix set carro

--- RacingManager/business/Jogador.java
+++ RacingManager/business/Jogador.java
@@ -57,7 +57,7 @@ public class Jogador implements Comparable<Jogador>,Serializable{
     }
 
     public void setCarro(Carro Carro){
-        this.carro = carro;
+        this.carro = Carro;
     }
 
     //Piloto
